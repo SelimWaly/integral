d84037edd629eaaf9fe20bb7281562eee19a6c6a
Aron Petkovski <aronpetko@gmail.com>
2024-05-04 17:16:17 -0500

    Refactors + Separate eval/static eval (22 elo)

--- src/chess/uci.cc
+++ src/chess/uci.cc
@@ -8,6 +8,9 @@
 #include "../tests/tests.h"
 #include "move_gen.h"
 
+#define INT_OPTION(name, value, min, max) options[name] = Option(name, value, min, max)
+#define STRING_OPTION(name, value) options[name] = Option(name, value)
+
 namespace uci {
 
 std::unordered_map<std::string_view, Option> options;
@@ -17,17 +20,15 @@ Option &GetOption(std::string_view option) {
   return options[option];
 }
 
-void CreateOption(std::string_view name,
-                  OptionType type,
-                  const std::any &value,
-                  int min = 0,
-                  int max = 0) {
-  options[name] = Option(type, value, min, max);
+void InitializeOptions() {
+  INT_OPTION("Hash", 64, 0, 1024);
+  INT_OPTION("Threads", 1, 1, 1);
 }
 
-void InitializeOptions() {
-  CreateOption("Hash", OptionType::kSpin, 64, 0, 1024);
-  CreateOption("Threads", OptionType::kSpin, 1, 1, 1);
+void PrintOptions() {
+  for (const auto &[_, option] : options) {
+    std::cout << option.ToString() << std::endl;
+  }
 }
 
 void Position(Board &board, std::stringstream &input_stream) {
@@ -113,6 +114,7 @@ void AcceptCommands(int arg_count, char **args) {
   move_gen::InitializeAttacks();
 
   InitializeOptions();
+  PrintOptions();
 
   const int kTTMbSize = 64;
   transposition_table.Resize(kTTMbSize);
